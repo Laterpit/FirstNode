4956e40e869f7ab5c3879a6031f8f5995e0b8065
Laterpit <linus.wagberg@gmail.com>
2015-01-06 16:43:34 +0100

    Refresh

--- app.js
+++ app.js
@@ -3,3 +3,9 @@
  * Description: Main javascript file for first node app
  */
 
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
