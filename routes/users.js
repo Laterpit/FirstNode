dd931253c6d0ba6eef8d186a1191e18a70c71df3
Laterpit <linus.wagberg@gmail.com>
2015-01-06 18:32:59 +0100

    First real app version

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var faker = require('../node_modules/faker');
+var router = express.Router();
+
+function User(name, email, phone, avatar) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.avatar = avatar;
+};
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    var users = [];
+    for (i = 0; i < 1; i++) {
+        users.push(new User(faker.name.firstName() + ' ' + faker.name.lastName(), faker.internet.email(),
+            faker.phone.phoneNumber(), faker.image.avatar()));
+    }
+    ;
+    res.render('users', {users: users});
+});
+
+module.exports = router;
