1ad67119b4839e24ba299cb7614631a1ba1fe208
Laterpit <linus.wagberg@gmail.com>
2015-03-07 19:28:32 +0100

    committed file api.js

--- routes/api.js
+++ routes/api.js
@@ -4,16 +4,10 @@
 
 var express = require('express');
 var router = express.Router();
+var faker = require('../node_modules/faker');
 
 router.get('/', function (req, res) {
-    if (req.query.q == 'Viktor' || req.query.q == 'viktor' || req.query.q == 'Larsson' || req.query.q == 'larsson'
-        || req.query.q == 'Viktor Larsson' || req.query.q == 'viktor larsson' || req.query.q == 'Viktor larsson'
-        || req.query.q == 'viktor Larsson') {
-        res.status(200).json({name: 'Viktor Larsson', gender: 'unsertain', status: "Not Awesome"});
-    } else {
-        res.status(200).json({name: 'Linus', gender: 'Manly man', status: "Awesome as hell"});
-    }
-    ;
+    res.status(200).json({message:{text:faker.lorem.paragraph(),id:faker.internet.password()}})
 });
 
 module.exports = router;
